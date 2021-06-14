c4e6a81d9b83a694865f33c28123f1072c0f62c4
yuki <s1932058jl@s.chibakoudai.jp>
2021-06-14 17:16:36 +0900

    component

--- src/views/About.js
+++ src/views/About.js
@@ -1,7 +1,10 @@
 const about_template = `
-<Card :src="https://s-yukisato.github.io/mysite/src/assets/sample.png"></Card>
+<Card :src="https://s-yukisato.github.io/mysite/src/assets/sample.png" />
 `
 
 const About = {
+    components: {
+        Card
+    },
     template: about_template
 }
