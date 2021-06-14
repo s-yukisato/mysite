b145e797e70f49d8db21c914fbb15254bf54ad95
yuki <s1932058jl@s.chibakoudai.jp>
2021-06-14 19:58:26 +0900

    setup data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,13 @@
 const card_template = `
 <div class="card" style="width: 18rem;">
-  <img :src="src" class="card-img-top" alt="...">
+  <img :src="items.data.src" class="card-img-top" alt="...">
   <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <p class="card-text">{{ items.data.content }}</p>
   </div>
 </div>
 `
 
 const Card = {
-    template: card_template,
-    props: {
-        src: {
-            type: String,
-            required: true
-        }
-    }
+  props: ['items'],
+  template: card_template
 }
--- src/views/About.js
+++ src/views/About.js
@@ -1,10 +1,22 @@
 const about_template = `
-<Card :src="https://s-yukisato.github.io/mysite/src/assets/sample.png" />
+<Card :items="cardList" />
 `
 
 const About = {
     components: {
         Card
+    },
+    setup() {
+        const cardList = Vue.reactive({
+            data: {
+                title: "Progressing",
+                content: "Some quick example text to build on the card title and make up the bulk of the card's content.",
+                src: "https://s-yukisato.github.io/mysite/src/assets/sample.png"
+            }
+        })
+        return {
+            cardList
+        }
     },
     template: about_template
 }
